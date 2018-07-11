0a4cc5c97ab91da2e1c8429ce2708c6a90ad9523
Victor Velasquez <victorv24@gmail.com>
2018-07-10 18:12:59 -0700

    recommiting to the changes

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-#(document).ready(function() {
+$(document).ready(function() {
 
-	$('#content ul li:first-child').addClass('active')
-	$('#content section:first-child').show()
+	$('.header').on('click', function() {
 
-	$('#content ul li a').on('click', function() {
-		var id = $(this).attr('href') // gives the section id based on what we clicked 
+		$('.words').removeClass('active')
+		$(this).find(' + .words').addClass('active')
 
-		$('#content ul li a').removeClass('active') // Make sure other elements are not active
-		$(this).addClass('active') // Make sure current link is the only active one
-
-		$('#content section').slideUp() // Do this when hiding
-		$(id).slideDown() // Do this when showing
 	})
 
 })
